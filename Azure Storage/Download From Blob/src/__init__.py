6a2a62c13c3e54b7f684f9de9f8f1b0b722d351c
BIAdmin <bipartner3@viacom18.com>
2020-04-26 21:05:11 +0530

    azure download code complete

--- Azure Storage/Download From Blob/src/__init__.py
+++ Azure Storage/Download From Blob/src/__init__.py
@@ -1,4 +1,8 @@
+from  download_from_azure_blob_stream import *
+
 blob_con_string= 'DefaultEndpointsProtocol=https;AccountName=vikashstorage;AccountKey=1psvKXn97QY89JUsBQ/54209m0RvveBjkbqkAqjuU5fuJFOzgTbBExCZy8smdL+xxytSowTpHBOyUdM9eJtSbg==;EndpointSuffix=core.windows.net'
 container_name= 'mycontainer'
 blob_name='empdata.csv'
-download_file_path='dowloaded_data_file.csv'
+download_file='dowloaded_data_file.csv'
+
+download(blob_con_string,container_name,blob_name,download_file)
--- Azure Storage/Download From Blob/src/download_from_azure_blob_stream.py
+++ Azure Storage/Download From Blob/src/download_from_azure_blob_stream.py
@@ -2,10 +2,10 @@ from azure.storage.blob import BlockBlobService
 from io import BytesIO
 from io import StringIO
 
-def download(blob_con_string,container_name,blob_name,download_file_path):
+def download(blob_con_string,container_name,blob_name,download_file):
     blob_service =  BlockBlobService(connection_string=blob_con_string)
-    stream = BytesIO()
-    download_data = blob_service.get_blob_to_text(container_name=container_name,blob_name=blob_name).content
-    with open(download_file_path, "w+") as download_file:
-        download_file.write(download_data)
-    download_file.close()
+    data_stream = BytesIO()
+    blob_service.get_blob_to_stream(container_name=container_name,blob_name=blob_name,stream=data_stream)
+    with open(download_file,'wb') as out:
+        out.write(data_stream.getvalue())
+    out.close()
